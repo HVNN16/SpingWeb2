962f88874ec8a3ffafd4789ef939c4484495c5dc
MinhNguyet-Love <minhnguyet22072004@gmail.com>
2025-05-17 14:29:10 +0700

    Cập nhật giao diện danh sách và chi tiết thành viên

--- src/main/java/donga/edu/demo/controllers/demo.java
+++ src/main/java/donga/edu/demo/controllers/demo.java
@@ -3,16 +3,29 @@ package donga.edu.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class demo {
+
+    private final List<String> members = Arrays.asList(
+            "Huỳnh Văn Nghĩa",
+            "Nguyễn Thị Minh Nguyệt",
+            "Nguyễn Trung Kỳ"
+    );
+
     @GetMapping("/members")
     public String showMemberList(Model model) {
-        List<String> members = Arrays.asList("Huỳnh Văn Nghĩa", "Nguyễn Thị Minh Nguyệt", "Nguyễn Trung Kỳ");
         model.addAttribute("members", members);
         return "member-list";
+    }
+
+    @GetMapping("/members/{name}")
+    public String showMemberDetail(@PathVariable("name") String name, Model model) {
+        model.addAttribute("memberName", name);
+        return "member-detail";
     }
 }
