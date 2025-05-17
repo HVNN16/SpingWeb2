f113b3ef31423c40981d3531e795a89271d62ba0
Văn Nghĩa - Love <127735777+HVNN16@users.noreply.github.com>
2025-05-17 14:38:07 +0700

    fix controllers

--- src/main/java/donga/edu/demo/controllers/demo.java
+++ src/main/java/donga/edu/demo/controllers/demo.java
@@ -1,18 +1,35 @@
 package donga.edu.demo.controllers;
 
+import donga.edu.demo.models.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class demo {
+
+    private final List<Member> members = List.of(
+            new Member("Huỳnh Văn Nghĩa", 25, "Leader", "nghia@donga.edu"),
+            new Member("Nguyễn Thị Minh Nguyệt", 22, "Developer", "nguyet@donga.edu"),
+            new Member("Nguyễn Trung Kỳ", 23, "Designer", "ky@donga.edu")
+    );
+
     @GetMapping("/members")
     public String showMemberList(Model model) {
-        List<String> members = Arrays.asList("Huỳnh Văn Nghĩa", "Nguyễn Thị Minh Nguyệt", "Nguyễn Trung Kỳ");
         model.addAttribute("members", members);
         return "member-list";
+    }
+
+    @GetMapping("/members/{name}")
+    public String showMemberDetail(@PathVariable("name") String name, Model model) {
+        Member memberDetail = members.stream()
+                .filter(member -> member.getName().equals(name))
+                .findFirst()
+                .orElse(null);
+        model.addAttribute("member", memberDetail);
+        return "member-detail";
     }
 }
